aac080761d21359540fd5142fdc8d0615da47337
fsmithred <fsmithred@gmail.com>
2011-07-24 10:15:00 -0400

    create/manage picture gallery html file

---
+++ add_pics_to_html_10.pl
@@ -0,0 +1,154 @@
+#!/usr/bin/env perl
+# add_pics_to_html_10.pl
+# copyright 2011, m. tornow and fsmithred
+# lots of help by telemachus and dbbolton too
+# license: GPL-3
+
+
+#--------------------------------------------------#
+# USE
+use warnings;
+use 5.012_003;
+use autodie;
+use File::Copy;
+use Cwd;
+#--------------------------------------------------#
+
+
+
+#--------------------------------------------------#
+# VARIABLES
+my $work_dir = "$ENV{HOME}/public_html/gallery";
+my $pictures_dir = "pictures";
+my $thumbnails_dir = "thumbnails";
+my $gallery_html = "gallery.html";
+my $url = "http://localhost/$ENV{USER}/gallery";
+
+
+chdir ("$work_dir/$pictures_dir");
+my @picture_folders = glob("*");
+chdir ("$work_dir/$thumbnails_dir");
+my @thumbnail_folders = glob("*");
+chdir ("$work_dir");
+
+#--------------------------------------------------#
+
+
+
+#--------------------------------------------------#
+# First of all: make a BACKUP
+if ( -f $gallery_html ) {
+	copy("$work_dir/$gallery_html","$work_dir/$gallery_html.orig");
+}
+#--------------------------------------------------#
+
+
+
+# OPEN  for WRITING
+open(my $out, ">", "$work_dir/$gallery_html");
+#--------------------------------------------------#
+
+
+
+#--------------------------------------------------#
+# WRITE  HEADER
+my $header = << "EOF";
+<html>
+<head>
+<title>Gallery</title>
+<meta charset="utf-8">
+</head>
+
+<body>
+<h1>My pictures</h1>
+<p>Some words of wisdom here. Whatever you might want. </p>
+EOF
+
+
+# Print the header to file.
+print $out $header;
+#--------------------------------------------------#
+
+
+
+#--------------------------------------------------#
+
+# WORK
+# Do the Voodoo:
+# for each folder in gallery/{thumbnails,pictures}
+# make a heading, and add the thumbnails/pictures to our gallery
+foreach my $folder (@picture_folders) {
+   chdir ("$work_dir/$pictures_dir/$folder");
+   my @pictures = glob("*");
+   
+   say $out "<h2>$folder</h2>";
+   
+   # Create the folder for the thumbnails if it doesn't exist
+   unless ( -d "$work_dir/$thumbnails_dir/$folder" ) {
+	   say "Creating directory: $work_dir/$thumbnails_dir/$folder";
+	   system "mkdir -p $work_dir/$thumbnails_dir/$folder";
+   }
+   
+   foreach my $pic  (@pictures) {
+	   # create thumbnail if it doesn't already exist
+	   if ( -f "$work_dir/$thumbnails_dir/$folder/$pic" ) {
+		   my $line = "<a href=\"$url/$pictures_dir/$folder/$pic\"><img src=\"$url/$thumbnails_dir/$folder/$pic\" alt=\"Desciption\"></a>";
+		   say $out $line;
+	   }
+	   else {
+		say "Adding thumbnail for $folder/$pic";
+		system "convert -resize 145x145 $work_dir/$pictures_dir/$folder/$pic $work_dir/$thumbnails_dir/$folder/$pic" ;
+		my $line = "<a href=\"$url/$pictures_dir/$folder/$pic\"><img src=\"$url/$thumbnails_dir/$folder/$pic\" alt=\"Desciption\"></a>";
+		   say $out $line;
+	   }
+   }
+}
+
+# Clean up orphaned thumbnail folders and files
+foreach my $folder (@thumbnail_folders) {
+	chdir ("$work_dir/$thumbnails_dir/$folder");
+	my @thumbnails = glob("*");
+
+# Remove orphaned thumbnail folders
+# Stop reading here, and go get more coffee.
+#
+# If there aren't any orphaned folders, then go through and remove
+# orphaned thumbnail files.	
+	unless ( -d "$work_dir/$pictures_dir/$folder" ) {
+		say "Removing directory: $$work_dir/thumbnails_dir/$folder";
+		system "rm -r $work_dir/$thumbnails_dir/$folder";
+	}
+	else {
+		# Remove orphaned thumbnail files
+		foreach my $pic (@thumbnails) {
+			unless ( -f "$work_dir/$pictures_dir/$folder/$pic" ) {
+				say "Removing thumbnail for $folder/$pic";
+				system (`rm "$work_dir/$thumbnails_dir/$folder/$pic"`);
+			}
+		}
+	}
+}
+
+#--------------------------------------------------#
+
+
+
+
+#--------------------------------------------------#
+# FOOTER
+my $footer = << "EOF";
+<p>Powered by Debian, perl and vim</p>
+</body>
+</html>
+EOF
+
+# PRINT the FOOTER
+print $out $footer;
+#--------------------------------------------------#
+
+
+
+#--------------------------------------------------#
+# DONE
+close $out;
+#--------------------------------------------------#
